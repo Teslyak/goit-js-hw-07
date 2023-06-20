7026ce5c61bc6694aceaabf9e6a3a3ee37d13f2a
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-06-20 11:04:56 +0300

    hw7-task1-

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryListItems = document.querySelector('.gallery');
+console.log(galleryListItems);
+
+const makeTagsGallery = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="Image ${description}"
+    />
+  </a>
+</li>`
+}).join('');
+ console.log(makeTagsGallery);
+
+galleryListItems.insertAdjacentHTML('beforeend', makeTagsGallery);
