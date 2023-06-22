bff667ce027c13bc41e1171e9ae87ac39894a66c
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-06-22 15:51:42 +0300

    hw-task2-

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  ul: document.querySelector('.gallery'),
+  
+}
+
+const createGallery = makeTagsGallery(galleryItems);
+
+function makeTagsGallery(img) {
+    return img.map(({ preview, original, description }) => { 
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="#">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="Image ${description}"
+    />
+  </a>
+</li>`
+    }).join('');
+   
+};
+ 
+refs.ul.insertAdjacentHTML('beforeend', createGallery);
+
+refs.ul.addEventListener('click', onClickItemGallery);
+
+
+function onClickItemGallery(event) {
+  if (event.target.classList.contains('gallery')) { 
+      return;
+  };
+    
+};
+
+let galleryNew = new SimpleLightbox('.gallery__item a');
+
